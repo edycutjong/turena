c5d83957904cdbbd4794e729ee3952b18e486713
Edy Cu <edy.cu@live.com>
2026-05-01 22:10:45 +0700

    fix(contracts): load .env from project root instead of contracts dir
    - Change dotenv path to '../.env' so hardhat picks up shared env vars

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const deployerKey = process.env.DEPLOYER_PRIVATE_KEY ?? "0x" + "0".repeat(64);
 
